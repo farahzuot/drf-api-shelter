0ba65dd8bf69aa521f730da3293042a02877fed4
farahzuot <farahz997@eng.hu.edu.jo>
2021-01-10 21:40:26 +0200

    finished

--- cats/admin.py
+++ cats/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
+from .models import Cats_data
 # Register your models here.
+admin.site.register(Cats_data)
--- cats/views.py
+++ cats/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
+from .serializer import ShelterSerializer
+from .models import Cats_data
 # Create your views here.
+class ShelterView(ListAPIView):
+    queryset = Cats_data.objects.all()
+    serializer_class = ShelterSerializer
+
+class ShelterDetailsView(RetrieveAPIView):
+    queryset = Cats_data.objects.all()
+    serializer_class = ShelterSerializer
